e51ae5093c25627463a4a5439e3b3af195e4a94c
山口修平 <shuuhei@yamaguchishuuheinoMacBook-Pro.local>
2019-09-12 19:52:33 +0900

    fix conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :end_users
+  resources:items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
- get 'top' => 'admins#top'
-
-resources:items
+  
+get 'top' => 'admins#top'
+get 'mypage' => 'end_users#mypage'
+get 'quit' => 'end_users#quit'
 end
