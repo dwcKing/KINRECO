4b604c6e9e82605d3ccfbad313eba6ad494cca4d
Kai Hasegawa <fromnowon0922@gmail.com>
2019-09-18 19:09:40 +0900

    test

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2019_09_17_064755) do
 
-
-  create_table "discs", force: :cascade do |t|
-    t.integer "disc"
-    t.integer "item_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -37,11 +30,11 @@ ActiveRecord::Schema.define(version: 2019_09_17_064755) do
   end
 
   create_table "arrivals", force: :cascade do |t|
+    t.datetime "arrivals_date"
     t.integer "arrivals_quantity"
-    t.integer "item_id"
-    t.integer "arrivals_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "item_id"
   end
 
   create_table "authors", force: :cascade do |t|
@@ -72,22 +65,34 @@ ActiveRecord::Schema.define(version: 2019_09_17_064755) do
     t.integer "end_user_id"
   end
 
+  create_table "discs", force: :cascade do |t|
+    t.integer "disc"
+    t.integer "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "end_users", force: :cascade do |t|
-    t.string "email", default: "", null: false
+    t.string "email"
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.string "last_name"
     t.string "first_name"
     t.string "last_kana"
     t.string "first_kana"
-    t.string "phone_number"
     t.string "post_code"
     t.text "address"
+    t.string "phone_number"
     t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer "quit_status"
     t.index ["email"], name: "index_end_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_end_users_on_reset_password_token", unique: true
@@ -112,6 +117,18 @@ ActiveRecord::Schema.define(version: 2019_09_17_064755) do
     t.text "comment"
   end
 
+  create_table "items_images", force: :cascade do |t|
+    t.string "image_id"
+    t.integer "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "labels", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "labels_name"
+  end
 
   create_table "orders", force: :cascade do |t|
     t.integer "order_status"
@@ -125,18 +142,8 @@ ActiveRecord::Schema.define(version: 2019_09_17_064755) do
     t.string "phone_number"
     t.integer "payment"
     t.integer "end_user_id"
-
-  create_table "items_images", force: :cascade do |t|
-    t.string "image_id"
-    t.integer "item_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "labels", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "labels_name"
   end
 
   create_table "songs", force: :cascade do |t|
