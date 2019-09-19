689cdeee21a8d97d73bd25566fa47a7076b077b0
naoto N <n@MacBook-Pro-N.local>
2019-09-19 19:08:05 +0900

    [修正]マイグレーションファイル

--- db/migrate/20190916061409_create_items_images.rb
+++ db/migrate/20190916061409_create_items_images.rb
@@ -1,8 +1,8 @@
 class CreateItemsImages < ActiveRecord::Migration[5.2]
   def change
     create_table :items_images do |t|
-      t.references  :image_id, foreign_key: true
-      t.references :item_id, foreign_key: true
+      t.references  :image, foreign_key: true
+      t.references :item, foreign_key: true
 
       t.timestamps
     end
