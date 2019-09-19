272ef2fb8e8d947aac95b18463932802f6beca9b
naoto N <n@MacBook-Pro-N.local>
2019-09-19 19:38:02 +0900

    変更スキーマ

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_18_074605) do
+ActiveRecord::Schema.define(version: 2019_09_19_065447) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
