0175155c010362c61affc422ad5b02d7d9dd153d
Kai Hasegawa <fromnowon0922@gmail.com>
2019-09-19 19:34:59 +0900

    enduser_view

--- app/controllers/users/end_users_controller.rb
+++ app/controllers/users/end_users_controller.rb
@@ -29,11 +29,7 @@ class Users::EndUsersController < ApplicationController
 
   def edit
   end
-<<<<<<< HEAD
 
-=======
-  
->>>>>>> master
   def destinations
     @order = Order.new
     @destination = Destination.all
